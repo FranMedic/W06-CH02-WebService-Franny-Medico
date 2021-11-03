887fb49cbb930ed996ea1cc6bbef256c2a5d6ffc
Franny Medico <m.francisca.medico@gmail.com>
2021-11-03 20:44:07 +0100

    added commander to access with external port, and pathcheck

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 require("dotenv").config();
 
+const { program } = require("commander");
 const http = require("http");
 const url = require("url");
 const calculator = require("./calculator");
 
+program.option("-p, --port <port>");
+program.parse(process.argv);
+
 const server = http.createServer();
-const port = process.env.SERVER_PORT_CALCULATOR || 6000;
+
+let port;
+
+if (program.opts().port && !Number.isNaN(program.opts().port)) {
+  port = program.opts().port;
+} else {
+  port = process.env.SERVER_PORT_CALCULATOR || 6000;
+}
 
 server.listen(port);
 
 server.on("request", (request, response) => {
-  console.log("Una request salvaje apareció");
-
-  const { a, b } = url.parse(request.url, true).query;
+  const parsedUrl = url.parse(request.url, true);
+  const { a, b } = parsedUrl.query;
   response.setHeader("Content-Type", "text/html");
-  response.write(calculator(a, b));
-  console.log("Me voy a ir yendo");
+
+  if (
+    parsedUrl.pathname === "/calculator/" ||
+    parsedUrl.pathname === "/calculator"
+  ) {
+    response.write(calculator(a, b));
+  } else {
+    response.write("<h1>CATACLISMO!!, ERROR!!, NO EXISTIMOS AIUDA!!!</h1>");
+  }
 
   response.end();
 });
