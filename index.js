3a4f707aea491125126c1855b306fdb4d2624fb0
Franny Medico <m.francisca.medico@gmail.com>
2021-11-03 20:06:14 +0100

    added html and passed query params to the calculator

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+require("dotenv").config();
+
 const http = require("http");
+const url = require("url");
+const calculator = require("./calculator");
 
 const server = http.createServer();
+const port = process.env.SERVER_PORT_CALCULATOR || 6000;
 
-server.listen(5000);
+server.listen(port);
 
 server.on("request", (request, response) => {
   console.log("Una request salvaje apareció");
-  response.setHeader("Content-Type", "application/json");
-  response.write("Holi toy biba");
-  console.log("llegue");
+
+  const { a, b } = url.parse(request.url, true).query;
+  response.setHeader("Content-Type", "text/html");
+  response.write(calculator(a, b));
+  console.log("Me voy a ir yendo");
+
   response.end();
 });
